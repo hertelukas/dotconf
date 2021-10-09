4f70310f6e3a281ea2e6e8a23bccf8360775c0bb
hertelukas <luki.hertel@gmail.com>
2021-10-09 19:33:28 +0200

    Added Scripts

---
+++ Scripts/change-screen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+STATE=$(cat /home/lukas/LocalDocuments/Scripts/screen-state.txt)
+echo $STATE
+if [ "$STATE" = "off" ]; then
+	xrandr --output HDMI-0 --auto --left-of DP-0
+	echo "on" > /home/lukas/LocalDocuments/Scripts/screen-state.txt
+else
+	xrandr --output HDMI-0 --off
+	echo "off" > /home/lukas/LocalDocuments/Scripts/screen-state.txt
+fi
