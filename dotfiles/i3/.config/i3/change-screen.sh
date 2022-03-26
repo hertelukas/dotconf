7dbb01798f1cc2aafbb5c28b34ccaa76dc6027d5
lukas <luki.hertel@gmail.com>
2022-03-26 16:37:21 +0100

    Minor improvements

--- dotfiles/i3/.config/i3/change-screen.sh
+++ dotfiles/i3/.config/i3/change-screen.sh
@@ -2,7 +2,7 @@
 STATE=$(cat /home/lukas/LocalDocuments/Scripts/screen-state.txt)
 echo $STATE
 if [ "$STATE" = "off" ]; then
-	xrandr --output HDMI-0 --auto --left-of DP-0
+	xrandr --output HDMI-0 --auto --left-of DP-0 --priamry
 	echo "on" > /home/lukas/LocalDocuments/Scripts/screen-state.txt
 else
 	xrandr --output HDMI-0 --off
